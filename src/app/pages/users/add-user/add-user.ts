4670b16ac6c7f9dd1b4ffa330792aaaf5356f635
rafa <garciacambronrafael@gmail.com>
2026-03-23 13:35:23 +0100

    termino modulos  add edit y delete y list

--- src/app/pages/users/add-user/add-user.ts
+++ src/app/pages/users/add-user/add-user.ts
@@ -1,11 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Usuario } from '../../../models/usuario';
+import { UsersCrud } from '../../../services/users-crud';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { RegisterService } from '../../../services/registerService';
 
 @Component({
   selector: 'app-add-user',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, MatSnackBarModule],
   templateUrl: './add-user.html',
-  styleUrl: './add-user.css',
+  styleUrls: ['./add-user.css']
 })
 export class AddUser {
 
+  nuevoUsuario: Omit<Usuario, 'id_usuario'> = {
+    nombre: '',
+    email: '',
+    password: '',
+    rol: 0,
+    dni: '',
+    direccion: '',
+    telefono: ''
+  };
+
+  @Output() agregado = new EventEmitter<void>(); // Avisar al padre
+  @Output() cancel = new EventEmitter<void>();
+
+  constructor(private register: RegisterService, private snackBar: MatSnackBar) {}
+
+  private showMessage(message: string, success: boolean = true) {
+    this.snackBar.open(message, 'Cerrar', {
+      duration: 3000,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom',
+      panelClass: success ? ['snackbar-success'] : ['snackbar-error']
+    });
+  }
+
+  agregarUsuario() {
+    this.register.create(this.nuevoUsuario).subscribe({
+      next: () => {
+        this.showMessage('Usuario agregado correctamente');
+        this.agregado.emit();
+        this.nuevoUsuario = { nombre:'', email:'', password:'', dni:'', rol:0, direccion:'', telefono:'' }; // reset
+      },
+      error: () => this.showMessage('Error al agregar usuario', false)
+    });
+  }
+
+  cancelar() {
+    this.cancel.emit();
+  }
 }
--- src/app/pages/users/delete-user/delete-user.ts
+++ src/app/pages/users/delete-user/delete-user.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Usuario } from '../../../models/usuario';
+import { UsersCrud } from '../../../services/users-crud';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-delete-user',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatSnackBarModule],
   templateUrl: './delete-user.html',
-  styleUrl: './delete-user.css',
+  styleUrls: ['./delete-user.css'],
 })
 export class DeleteUser {
 
+  @Input() usuario!: Usuario;       // Usuario a eliminar
+  @Output() eliminado = new EventEmitter<void>(); // Emitir cuando se elimina
+  @Output() cancel = new EventEmitter<void>();   // Emitir cuando se cancela
+
+  constructor(private usersCrud: UsersCrud, private snackBar: MatSnackBar) {}
+
+  private showMessage(message: string, success: boolean = true) {
+    this.snackBar.open(message, 'Cerrar', {
+      duration: 3000,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom',
+      panelClass: success ? ['snackbar-success'] : ['snackbar-error']
+    });
+  }
+
+  confirmarEliminacion() {
+    if (!this.usuario) return;
+
+    this.usersCrud.deleteUsuario(this.usuario.id_usuario).subscribe({
+      next: () => {
+        this.showMessage('Usuario eliminado correctamente');
+        this.eliminado.emit();
+      },
+      error: () => this.showMessage('Error al eliminar el usuario', false)
+    });
+  }
+
+  cancelar() {
+    this.cancel.emit();
+  }
 }
